ed828c16fc804bf553f7c2b1a21e8a3698073910
Arfa01 <79276565+Arfa01@users.noreply.github.com>
2026-08-05 04:27:39 +0500

    corrected comment
    Updated comments for clarity in vite.config.ts.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,12 @@
-import tailwindcss from '@tailwindcss/vite' 
-import react from '@vitejs/plugin-react' 
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    proxy: {                  // development proxy forwards browser reqs from /api on 5173 to to backend at 5000
+    proxy: {
+      // Proxy API requests to Express during local development.
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
